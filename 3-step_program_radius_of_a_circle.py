f99fd507179aa00887c9c5ede61b3127822be14e
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-10-31 21:44:54 -0400

    Add files via upload
    starting off with a four step program to solve for area, and combining the float and input to get a three-step program.

---
+++ 3-step_program_radius_of_a_circle.py
@@ -0,0 +1,11 @@
+#below is an initial given four-step program to compute area
+response = input("what is your radius ?")
+r = float(response)
+area = 3.14159 * r**2
+print("The area is ", area)
+
+#I combined the float with input to shorten the steps
+#It works the same as the one above
+radius = float(input("The radius is "))
+area = 3.1415 * radius**2
+print ("The area is ", area)
